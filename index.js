4b7ffd6ba934875d710da9fa0499315c4e5288af
Antony Kahuro <kahuro91tony@gmail.com>
2023-07-06 21:30:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(act="roller-skate"){
+    return `This Saturday, I want to ${act}!`;
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+
+function wrapAdjective(flair="*"){
+    return function(name="special"){
+        return `You are ${flair}${name}${flair}!`;
+    }
+}
+
+
+//console.log(saturdayFun());
